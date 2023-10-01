5b290100742cd7d6ec3e1ccb49d89b9438073247
JHwan96 <dlwnghks00@gmail.com>
2023-10-02 08:39:24 +0900

    practice java spring

--- Inflearn/core/core/src/main/java/hello/core/AutoAppConfig.java
+++ Inflearn/core/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -8,8 +11,12 @@ import org.springframework.stereotype.Component;
 @Configuration
 @ComponentScan(
         basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes=Configuration.class)
 )
 public class AutoAppConfig {
-
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memoryRepository(){
+//        return new MemoryMemberRepository();
+//    }
 }
