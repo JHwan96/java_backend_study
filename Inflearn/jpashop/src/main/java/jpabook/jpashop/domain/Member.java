84b877cf06929adc7f84bcc4b1f508be5231522b
JHwan96 <dlwnghks00@gmail.com>
2023-10-07 23:43:24 +0900

    practice java spring : entity

---
+++ Inflearn/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Member {
+    @Id @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+
+    private String name;
+
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+}
