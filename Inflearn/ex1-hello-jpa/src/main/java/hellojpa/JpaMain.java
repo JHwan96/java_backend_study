be44b13b44eaaa75fefc85d2f243fcd787977def
JHwan96 <dlwnghks00@gmail.com>
2023-10-16 12:15:30 +0900

    feat: member CRUD 연습
    등록, 수정, 삭제, 조회

--- Inflearn/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Inflearn/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -13,11 +13,15 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("helloB");
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("helloB");
+//            em.persist(member);       // 멤버 추가
 
-            em.persist(member);
+//            em.remove(findMember); // 멤버 삭제
+            Member findMember = em.find(Member.class, 1L); // 멤버 검색
+            findMember.setName("helloJPA"); // 멤버 수정 (update해줌)
+            
             tx.commit();
         } catch (Exception e){
             tx.rollback();
