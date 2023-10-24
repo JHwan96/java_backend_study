c4cde7b64f7638a31213a4513f036535858c59a7
JHwan96 <dlwnghks00@gmail.com>
2023-10-24 11:16:11 +0900

    프록시 예제 연습

--- Inflearn/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Inflearn/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,11 +1,8 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import java.time.LocalDateTime;
-import java.util.List;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
 
 public class JpaMain {
     public static void main(String[] args){
@@ -15,23 +12,39 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member();
-            member.setUsername("user1");
-            member.setCreateBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
-
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("hello");
+            em.persist(member1);
 
             em.flush();
             em.clear();
 
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("reference.getClass() = " + refMember.getClass());   // proxy
+
+//            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));   // 초기화 여부 확인
+
+            Hibernate.initialize(refMember);    // 프록시 강제 초기화
+
+          /*em.detach(refMember);
+            em.clear(); // 준영속 상태일 때 에러 확인
+            refMember.getUsername();*/
+
+            /*Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findmember.getClass() = " + findMember.getClass());
+            System.out.println("a == a: " + (refMember == findMember));*/   // == 연산자 확인
             tx.commit();
         } catch (Exception e){
-            System.out.println(e);
+            e.printStackTrace();
             tx.rollback();
         } finally{
             em.close();
         }
         emf.close();
+    }
+
+    private static void logic(Member m1, Member m2) {
+        System.out.println("m1 == m2: " + (m1 instanceof Member));
+        System.out.println("m1 == m2: " + (m2 instanceof Member));
     }
 }
--- Inflearn/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ Inflearn/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -14,18 +14,9 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String username;
 
-    @OneToOne
-    @JoinColumn(name="LOCKER_ID")
-    private Locker locker;
-
     @ManyToOne  // 일대다 양방향
     @JoinColumn(name="TEAM_ID", insertable = false, updatable = false)
     private Team team;
-
-    @OneToMany(mappedBy = "member") // 다대다 연관관계
-    private List<MemberProduct> memberProducts = new ArrayList<>();
-
-
 
     public void setId(Long id) {
         this.id = id;
