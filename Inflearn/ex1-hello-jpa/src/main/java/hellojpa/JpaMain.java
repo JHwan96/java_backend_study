e09e4d20bea5311f5ce56b00cb19a34eb70165c6
JHwan96 <dlwnghks00@gmail.com>
2023-10-25 17:02:19 +0900

    컬렉션 값 타입 연습
    1. git commit 연습
    2. 수정할 때의문제로 일대다 관계를 대안으로 사용하는 것이 좋음

--- Inflearn/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Inflearn/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,6 @@
 package hellojpa;
 
-import org.hibernate.Hibernate;
-
 import javax.persistence.*;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args){
@@ -13,11 +10,33 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member= new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "100"));
-            member.setWorkPeriod(new Period());
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(new Address("homeCity1", "street","10000"));
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new Address("old1", "street","10000"));
+            member.getAddressHistory().add(new Address("old2", "street","10000"));
+
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("=============== START ===============");
+            Member findMember = em.find(Member.class, member.getId());
+//            findMember.getAddressesHistory().setCity("") // 이렇게 하면 안됨
+            Address oldAddress = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", oldAddress.getStreet(), oldAddress.getZipcode()));
+
+            // 치킨 -> 한식
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+            findMember.getAddressHistory().remove(new Address("old1", oldAddress.getStreet(), oldAddress.getZipcode()));
+            findMember.getAddressHistory().add(new Address("newCity", oldAddress.getStreet(), oldAddress.getZipcode()));
 
             tx.commit();
         } catch (Exception e){
--- Inflearn/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ Inflearn/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,9 +1,10 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member {
@@ -21,17 +22,33 @@ public class Member {
     // 주소
     @Embedded
     private Address homeAddress;
+    @ElementCollection
+    @CollectionTable(name="FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
 
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name="city", column=@Column(name="WORK_CITY")),
-            @AttributeOverride(name="street", column=@Column(name="WORK_STREET")),
-            @AttributeOverride(name="zipcode", column=@Column(name="WORK_ZIPCODE"))
-    })
-    private Address workAddress;
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<Address> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<Address> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 
     public void setId(Long id) {
