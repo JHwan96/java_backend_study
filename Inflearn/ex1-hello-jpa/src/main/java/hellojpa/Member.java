c8d77baa040ce6026cfb8faeffb961dc7fe3f264
JHwan96 <dlwnghks00@gmail.com>
2023-10-25 15:22:24 +0900

    임베디드 타입 연습
    1. Embedded, Embeddable
    2. AttributeOverride

--- Inflearn/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ Inflearn/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Member extends BaseEntity {
+public class Member {
 
     @Id @GeneratedValue
     @Column(name="MEMBER_ID")
@@ -14,31 +14,51 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.LAZY)  // 일대다 양방향
-    @JoinColumn(name="TEAM_ID")
-    private Team team;
+    // 기간 Period
+    @Embedded
+    private Period workPeriod;
 
-    public void setTeam(Team team) {
-        this.team = team;
-    }
+    // 주소
+    @Embedded
+    private Address homeAddress;
 
-    public Team getTeam() {
-        return team;
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="city", column=@Column(name="WORK_CITY")),
+            @AttributeOverride(name="street", column=@Column(name="WORK_STREET")),
+            @AttributeOverride(name="zipcode", column=@Column(name="WORK_ZIPCODE"))
+    })
+    private Address workAddress;
+
+    public Long getId() {
+        return id;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public Long getId() {
-        return id;
+    public Period getWorkPeriod() {
+        return workPeriod;
     }
 
-    public String getUsername() {
-        return username;
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
     }
 }
