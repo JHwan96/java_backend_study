eb825d1ebdb02a227272203a32f2925c0a933ca4
JHwan96 <dlwnghks00@gmail.com>
2023-10-29 23:09:29 +0900

    JPQL 벌크연산 예제 연습
    1. git commit 연습

--- Inflearn/jqpl/src/main/java/jpql/JpaMain.java
+++ Inflearn/jqpl/src/main/java/jpql/JpaMain.java
@@ -33,15 +33,13 @@ public class JpaMain {
            member3.setTeam(teamB);
            em.persist(member3);
 
-           em.flush();
-           em.clear();
 
-           List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                   .setParameter("username", member1.getUsername())
-                   .getResultList();
-           for (Member member : resultList){
-               System.out.println("member :" + member);
-           }
+           // em.flush() 자동 호출: commit, query, 강제 호출(em.flush())
+           int resultCount = em.createQuery("update Member m set m.age = 20")
+                   .executeUpdate();
+           em.clear();
+           Member findMember = em.find(Member.class, member1.getId());
+           System.out.println("findMember: " + findMember.getAge());
 
 
            tx.commit();
