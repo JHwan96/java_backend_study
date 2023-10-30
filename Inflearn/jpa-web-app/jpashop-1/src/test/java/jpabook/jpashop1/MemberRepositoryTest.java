13c1fd30d453f9e93589d73f1b5e6f8035d8fa05
JHwan96 <dlwnghks00@gmail.com>
2023-10-30 22:44:03 +0900

    chore: 라이브러리 및 gitignore 추가
    1. 라이브러리, application.yml 추가
    2. .gitignore 내용 추가
    3. 제대로 실행되는지 확인

---
+++ Inflearn/jpa-web-app/jpashop-1/src/test/java/jpabook/jpashop1/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop1;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void findMember(){
+        // given
+        Member member =  new Member();
+        member.setUsername("memberA");
+
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+
+}
