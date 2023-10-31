6e9d3a959038f1fb838d0158889ad891f723d235
JHwan96 <dlwnghks00@gmail.com>
2023-10-31 18:24:53 +0900

    feat: 연관관계 메소드 및 지연로딩 추가

--- Inflearn/jpa-web-app/jpashop-1/src/main/java/jpabook/jpashop1/domain/Delivery.java
+++ Inflearn/jpa-web-app/jpashop-1/src/main/java/jpabook/jpashop1/domain/Delivery.java
@@ -1,12 +1,16 @@
 package jpabook.jpashop1.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter @Setter
 public class Delivery {
     @Id @GeneratedValue
     private Long id;
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
     @Embedded
     private Address address;
--- Inflearn/jpa-web-app/jpashop-1/src/main/java/jpabook/jpashop1/domain/Order.java
+++ Inflearn/jpa-web-app/jpashop-1/src/main/java/jpabook/jpashop1/domain/Order.java
@@ -15,15 +15,31 @@ public class Order {
     @Id @GeneratedValue
     @Column(name="order_id")
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="member_id")
     private Member member;
-    @OneToMany(mappedBy="order")
-    private List<OrderItem> orderItem = new ArrayList<>();
-    @OneToOne
+    @OneToMany(mappedBy="order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+    @OneToOne(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
     private LocalDateTime orderDate;
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    // 연관관게 편입 메서드
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrder().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
