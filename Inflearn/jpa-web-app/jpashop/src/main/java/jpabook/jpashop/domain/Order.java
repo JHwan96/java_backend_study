2d90dee5f5f8ed898a10b287452fd9b9aa779348
JHwan96 <dlwnghks00@gmail.com>
2023-11-17 18:05:37 +0900

    feat: 주문 조회 예제 연습2
    Sec 4.
    - 주문 조회 V3.1~6
    - DTO 직접 조회

--- Inflearn/jpa-web-app/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ Inflearn/jpa-web-app/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -26,6 +27,7 @@ public class Order {
 
     @OneToMany(mappedBy="order", cascade=CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
+
     @OneToOne(fetch=FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name="delivery_id")
     private Delivery delivery;
