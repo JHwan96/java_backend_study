7ba7089a4f6f81ba9afb83ec3ff3b2165ff2ced1
JHwan96 <dlwnghks00@gmail.com>
2023-11-28 23:24:31 +0900

    검증 처리 예제 연습 V1
    - 검증 직접 처리 (V1)
    - 숫자 대신 문자가 들어가는 경우는 처리하지 못함

---
+++ Inflearn/spring-mvc/validation/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,16 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.KOREA);
+		SpringApplication.run(ItemServiceApplication.class, args);
+	}
+
+}
