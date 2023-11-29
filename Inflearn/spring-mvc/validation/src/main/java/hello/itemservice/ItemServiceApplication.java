4cff8576c466dfaed99ef0e86a6ca1277dcff579
JHwan96 <dlwnghks00@gmail.com>
2023-11-30 02:06:53 +0900

    Validator 분리 예제 연습
    - InitBinder, Validated 어노테이션 예제 연습

--- Inflearn/spring-mvc/validation/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ Inflearn/spring-mvc/validation/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,16 +1,18 @@
 package hello.itemservice;
 
+import hello.itemservice.web.validation.ItemValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.Validator;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Locale;
 
 @SpringBootApplication
-public class ItemServiceApplication {
+public class ItemServiceApplication{
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.KOREA);
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
-
 }
