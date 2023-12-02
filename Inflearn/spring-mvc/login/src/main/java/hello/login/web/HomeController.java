6075b237bb9664b387b0fc53f84205b79ab8969e
JHwan96 <dlwnghks00@gmail.com>
2023-12-02 20:13:07 +0900

    로그인 처리 (세션) 예제 연습

--- Inflearn/spring-mvc/login/src/main/java/hello/login/web/HomeController.java
+++ Inflearn/spring-mvc/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,8 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionConst;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,22 +11,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
 //    @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLogin(@CookieValue(name="memberId", required = false) Long memberId, Model model){
         if(memberId == null){
             return "home";
@@ -39,17 +45,46 @@ public class HomeController {
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
+//    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest  request, Long memberId, Model model){
+        //세션 관리자에 저장된 회원 정보
+        Member member = (Member)sessionManager.getSession(request);
 
-    @PostMapping("/logout")
-    public String logout(HttpServletResponse response){
-        expireCookie(response, "memberId");
-        return "redirect:/";
+        //로그인
+        if(member==null){
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
     }
 
-    private static void expireCookie(HttpServletResponse response, String cookieName) {
-        Cookie cookie = new Cookie(cookieName, null);
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
+//    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest  request, Long memberId, Model model){
+        HttpSession session = request.getSession(false);
+        if(session == null){
+            return "home";
+        }
+        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        //세션에 회원 데이터가 없으면 home
+        if(loginMember==null){
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required=false) Member loginMember, Long memberId, Model model){
+        //세션에 회원 데이터가 없으면 home
+        if(loginMember==null){
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
 
 }
