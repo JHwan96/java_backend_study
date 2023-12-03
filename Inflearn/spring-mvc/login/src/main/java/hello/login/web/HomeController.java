69bd76410fa8af24a67401438d7d581fd4e12508
JHwan96 <dlwnghks00@gmail.com>
2023-12-03 21:48:53 +0900

    서블릿 필터 및 스프링 인터셉터 예제 연습
    Sec 7. 로그인 처리2 - 필터, 인터셉터

--- Inflearn/spring-mvc/login/src/main/java/hello/login/web/HomeController.java
+++ Inflearn/spring-mvc/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentResolver.Login;
 import hello.login.web.session.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
@@ -75,8 +76,18 @@ public class HomeController {
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required=false) Member loginMember, Long memberId, Model model){
+        //세션에 회원 데이터가 없으면 home
+        if(loginMember==null){
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Long memberId, Model model){
         //세션에 회원 데이터가 없으면 home
         if(loginMember==null){
             return "home";
