a53631e23c3ca0e288a7a13b1bc592f325bef5e6
JHwan96 <dlwnghks00@gmail.com>
2023-12-20 11:08:34 +0900

    mybatis 적용 연습

--- Inflearn/spring-db/itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ Inflearn/spring-db/itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -5,6 +5,7 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,8 +13,9 @@ import java.util.Optional;
 @Mapper
 public interface ItemMapper {
     void save(Item item);
-    void update(@Param("id") Long id, @Param("updateParam")ItemUpdateDto updateDto);
+    void update(@Param("id") Long id, @Param("updateParam")ItemUpdateDto updateParam);
 
+//    @Select("select id, item_name, price, quantity from item where id=#{id}")   //잘 사용하지 않음
     Optional<Item> findById(Long id);
 
     List<Item> findAll(ItemSearchCond itemSearch);
