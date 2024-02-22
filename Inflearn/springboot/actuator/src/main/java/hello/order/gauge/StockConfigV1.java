6ac3fe9666c12164c6d79b6dbbb54ef912e60a41
JHwan96 <dlwnghks00@gmail.com>
2024-02-22 19:32:51 +0900

    메트릭 등록 예제
    Sec 10. 모니터링 메트릭 활용
    - 메트릭 등록 (timer, @Timer)
    - 게이지

---
+++ Inflearn/springboot/actuator/src/main/java/hello/order/gauge/StockConfigV1.java
@@ -0,0 +1,34 @@
+package hello.order.gauge;
+
+import hello.order.OrderService;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class StockConfigV1 {
+    @Bean
+    public MyStockMetric myStockMetric(OrderService orderService, MeterRegistry meterRegistry){
+        return new MyStockMetric(orderService, meterRegistry);
+    }
+    static class MyStockMetric{
+        private OrderService orderService;
+        private MeterRegistry meterRegistry;
+
+        public MyStockMetric(OrderService orderService, MeterRegistry meterRegistry) {
+            this.orderService = orderService;
+            this.meterRegistry = meterRegistry;
+        }
+        @PostConstruct
+        public void init(){
+            Gauge.builder("my.stock", orderService, service -> {
+                log.info("stock gauge call");
+                return service.getStock().get();
+            }).register(meterRegistry);
+        }
+    }
+}
