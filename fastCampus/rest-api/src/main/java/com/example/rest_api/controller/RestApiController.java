e939d912fc5099275c4b724cfd3283ffa845210e
JHwan96 <dlwnghks00@gmail.com>
2024-05-21 23:41:58 +0900

    feat: PathVariable 실습 추가

--- fastCampus/rest-api/src/main/java/com/example/rest_api/controller/RestApiController.java
+++ fastCampus/rest-api/src/main/java/com/example/rest_api/controller/RestApiController.java
@@ -1,6 +1,7 @@
 package com.example.rest_api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class RestApiController {
     @GetMapping(path = "/hello")
-    public String hello(){
+    public String hello() {
         String html = "<html> <body> <h1> Hello Spring Boot </h1> </body </html>";
 
         return html;
+    }
+
+    @GetMapping("/echo/{msg}/{userId}")
+    public String echo(@PathVariable(name = "msg") String message, @PathVariable int userId) {
+        System.out.println("echo message : " + message + " " + userId);
+        return message.toUpperCase();
     }
 }
