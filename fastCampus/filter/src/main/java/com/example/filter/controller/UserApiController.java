a0b90aa470ef339ee96553e847e6e481b71f6af0
JHwan96 <dlwnghks00@gmail.com>
2024-05-28 17:38:12 +0900

    interceptor 연습
    - HandlerInterceptor를 통해 인터셉터 생성
    - WebMvcConfigurer에 추가

--- fastCampus/filter/src/main/java/com/example/filter/controller/UserApiController.java
+++ fastCampus/filter/src/main/java/com/example/filter/controller/UserApiController.java
@@ -1,8 +1,10 @@
 package com.example.filter.controller;
 
+import com.example.filter.interceptor.OpenApi;
 import com.example.filter.model.UserRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserApiController {
 
     @PostMapping
-    public UserRequest register(
-            @RequestBody UserRequest userRequest
-//            HttpEntity http
-    ) {
+    @OpenApi
+    public UserRequest register( @RequestBody UserRequest userRequest ) {
         log.info("{}", userRequest);
         return userRequest;
     }
+
+    @GetMapping("/hello")
+    public void hello() {
+        log.info("hello");
+    }
+
 }
